7b471ef03a6591bfc49696c60a2f60a3a8f186fc
menezesfelipee <sa.felipem@gmail.com>
2021-07-05 22:10:21 -0300

    update

--- main.py
+++ main.py
@@ -16,14 +16,18 @@ if __name__ == '__main__':
         escolha = int(input('O que você quer fazer? '))
         if escolha == 1:
             print('''\nEscolha uma das opções de lazer:
-            [ 1 ] Dormir (8h)
+            [ 1 ] Dormir (8h) // Ir ao restaurante
             [ 2 ] Ir ao parque com a esposa
             [ 3 ] Assistir Netflix\n''')
 
             lazer = int(input('O que você quer fazer? '))
             if lazer == 1:
-                relogio.avancaTempo(8, 10)
-                thiago.dormir()
+                if thiago.dinheiro > 100:
+                    relogio.avancaTempo(8, 10)
+                    thiago.dormir()
+                else:
+                    print('Você não tem dinheiro suficiente.')
+                    continue
             elif lazer == 2:
                 relogio.avancaTempo(3, 30)
                 thiago.passear()
