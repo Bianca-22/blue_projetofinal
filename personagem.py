b07c45864f2806672143b0de15369a48af512f8b
Felipe Menezes <85652598+menezesfelipee@users.noreply.github.com>
2021-07-06 22:35:54 -0300

    mudança na estrutura das funções

--- personagem.py
+++ personagem.py
@@ -42,22 +42,14 @@ class Personagem:
         else:
             return valor
 
-    def dormir(self):
-        self.__saude += 20
+    def atribuirValores(self, saude = 0, relacionamento = 0, dinheiro = 0, estresse = 0, xp = 0):
+        self.__saude += saude
+        self.__relacionamento += relacionamento
+        self.__dinheiro += dinheiro
+        self.__estresse += estresse
+        self.__xp += xp
         self.__saude = self._avaliar(self.__saude)
-        self.__estresse -= 50
-        self.__estresse = self._avaliar(self.__estresse)
-
-    def passear(self):
-        self.__relacionamento += 20
         self.__relacionamento = self._avaliar(self.__relacionamento)
-        self.__estresse -= 20
+        self.__dinheiro = self._avaliar(self.__dinheiro)
         self.__estresse = self._avaliar(self.__estresse)
-        self.__saude -= 30
-        self.__saude = self._avaliar(self.__saude)
-    
-    def netflix(self):
-        self.__estresse -= 20
-        self.__estresse = self._avaliar(self.__estresse)
-        self.__relacionamento -= 20
-        self.__relacionamento = self._avaliar(self.__relacionamento)
+        self.__xp = self._avaliar(self.__xp)
