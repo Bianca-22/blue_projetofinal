8936d4fd6990860683ac5086fa6fb2c0072c6c78
menezesfelipee <sa.felipem@gmail.com>
2021-07-08 20:55:53 -0300

    pequenas alterações

--- main_luiz.py
+++ main_luiz.py
@@ -19,101 +19,106 @@ if __name__ == '__main__':
         [ 5 ] Projetos pessoais\n
         O que você quer fazer? '''))
         
-        if escolha == 3:
-            estudo = int(input('''\nEscolha  uma das opções de estudo:
+        print('''\n\n[italic dark_cyan]Escolha  uma das opções de estudo:[/italic dark_cyan]\n
         [1] HTML
         [2] PYTHON
         [3] JAVA SCRIPT
         [4] C
-        [5] PHP\n
-        O que você quer fazer? '''))
+        [5] PHP\n''')
+
+        if escolha == 3:
+            estudo = int(input(' O que você quer fazer? '''))
 
             if estudo == 1:
-
-                                
+                print('\n\nESTUDANDO...')
+                sleep(2)
                 if opcao != 3:
-                    relogio.avancaTempo(8)
-                    thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=10)
-                    print('Paranbens! Você conseguiu assimilar boa parte do que foi ensinado!')
-
-                else:
                     relogio.avancaTempo(4)
-                    thiago.atribuirValores(saude=-10, estresse=-30, relacionamento=0, xp=0)
-                    print('Puxa vida! Acabou a energia não vou conseguir assisitir a aula nem estudar!')
-
+                    thiago.atribuirValores(estresse=15, relacionamento=20, xp=5)
+                    print('\nParabéns! Você conseguiu assimilar boa parte do que foi ensinado!')
+                else:
+                    relogio.avancaTempo(2)
+                    thiago.atribuirValores(estresse=30)
+                    print('\nPuxa vida! Acabou a energia e você não conseguiu assisitir a aula nem estudar!')
+                    avancar()
+                    continue
                 
             elif estudo == 2:
-
                 if thiago.xp >= 20:
-                    
+                    print('\n\nESTUDANDO...')
+                    sleep(2)
                     if opcao != 3:
-                        relogio.avancaTempo(8)
-                        thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=20)
-                        print('Você avançou em conhecimento, agora esta aprendendo uma nova liguagem!')
-
-                    else:
                         relogio.avancaTempo(4)
-                        thiago.atribuirValores(saude=-10, estresse=+30, relacionamento=-20, xp=0)
-                        print('Meu cachorro mastigou o cabo do meu monitor, não vou conseguir acompanhar a aula! Minha esposa ficou brava por que deixei o cachorro para fora da casa!')
-
+                        thiago.atribuirValores(estresse=15, xp=10)
+                        print('\nVocê avançou em conhecimento, agora está aprendendo uma nova liguagem!')
+                    else:
+                        relogio.avancaTempo(2)
+                        thiago.atribuirValores(estresse=30, relacionamento=-20)
+                        print('\nO cachorro mastigou o cabo do monitor e você não conseguiu acompanhar a aula! Sua esposa ficou brava porque você colocou o cachorro para fora de casa!')
+                    avancar()
+                    continue
                 else:
-                    print('Continue estudando para somar cada vez mais conhecimento!')
+                    print('\n\nVocê ainda não tem experiência suficiente para estudar isso! Continue estudando para somar cada vez mais conhecimento!')
+                    avancar()
                     continue
 
             elif estudo == 3:
-
-                if thiago.xp >= 50:
-
+                if thiago.xp >= 40:
+                    print('\n\nESTUDANDO...')
+                    sleep(2)
                     if opcao != 3:
-                        relogio.avancaTempo(8)
-                        thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=20)
-                        print('Olha só onde você conseguiu chegar! Continue com essa dedicação e terá muitas boas surpresas!')
-
-                    else:
                         relogio.avancaTempo(4)
-                        thiago.atribuirValores(saude=-10, estresse=+30, relacionamento=-10, xp=-10)
-                        print('Puxa vida, derramei um copo de café no meu note e ele não que mais ligar!')
-
+                        thiago.atribuirValores(estresse=15, xp=20)
+                        print('\nOlha só onde você conseguiu chegar! Continue com essa dedicação e terá muitas boas surpresas!')
+                    else:
+                        relogio.avancaTempo(2)
+                        thiago.atribuirValores(estresse=30, dinheiro=-150)
+                        print('\nPuxa vida, você derramou um copo de café no notebook e ele não que mais ligar!')
+                    avancar()
+                    continue
                 else:
-                    print('Continue estudando para somar cada vez mais conhecimento!')
+                    print('\n\nVocê ainda não tem experiência suficiente para estudar isso! Continue estudando para somar cada vez mais conhecimento!')
+                    avancar()
                     continue
 
             elif estudo == 4:
-
-                if thiago.xp >= 50:
-
+                if thiago.xp >= 80:
+                    print('\n\nESTUDANDO...')
+                    sleep(2)
                     if opcao != 3:
-                        relogio.avancaTempo(8)
-                        thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=20)
-                        print('Agora é hora de aplicar tudo que ja aprendeu, e iniciar uma nova joranda com mais uma linguagem!')
-
-                    else:
                         relogio.avancaTempo(4)
-                        thiago.atribuirValores(saude=-30, estresse=+50, relacionamento=-20, xp=-10)
-                        print('Invadiram minha residencia e levaram todo meu equipamento de trabalho!')
-
+                        thiago.atribuirValores(estresse=15, xp=40)
+                        print('\nSua jornada com essa nova linguagem está sendo incrível!')
+                    else:
+                        relogio.avancaTempo(2)
+                        thiago.atribuirValores(estresse=50, dinheiro=-1500)
+                        print('\nInvadiram sua residência e levaram todo seu equipamento de trabalho!')
+                    avancar()
+                    continue
                 else:
-                    print('Continue estudando para somar cada vez mais conhecimento!')
+                    print('\n\nVocê ainda não tem experiência suficiente para estudar isso! Continue estudando para somar cada vez mais conhecimento!')
+                    avancar()
                     continue
 
             if estudo == 5:
-
-                if thiago.xp >= 50:
-
+                if thiago.xp >= 160:
+                    print('\n\nESTUDANDO...')
+                    sleep(2)
                     if opcao != 3:
-                        relogio.avancaTempo(8)
-                        thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=20)
-                        print('Olha só, somando cada vez mais conhecimento, agora chegou a hora de aumentar sua bagagem!')
-
-                    else:
                         relogio.avancaTempo(4)
-                        thiago.atribuirValores(saude=-10, estresse=30, relacionamento=20, xp=-10)
-                        print('Precisei sair as pressas para levar minha esposa ao médico.')
-
-                else:
-                    print('Continue estudando para somar cada vez mais conhecimento!')
+                        thiago.atribuirValores(estresse=15, xp=80)
+                        print('\nParabéns!! Se continuar assim você vai longe!')
+                    else:
+                        relogio.avancaTempo(2)
+                        thiago.atribuirValores(estresse=30, relacionamento=20)
+                        print('\nSua esposa se sentiu mal e você precisou sair às pressas para levá-la ao hospital.')
+                    avancar()
                     continue
-                   
+                else:
+                    print('\n\nVocê ainda não tem experiência suficiente para estudar isso! Continue estudando para somar cada vez mais conhecimento!')
+                    avancar()
+                    continue
+
         if thiago.saude == 0:
             print('GAME OVER')
             break
