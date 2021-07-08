cd96b41ab701d597145b1d3a6a3fed87d1778a21
luizscoimbra <53239828+luizscoimbra@users.noreply.github.com>
2021-07-07 22:01:44 -0300

    Commit 02 -  Alteração no codigo
    Alteração código.

--- main_luiz.py
+++ main_luiz.py
@@ -1,5 +1,6 @@
 from personagem import Personagem
 from relogio import Relogio
+from random import randint
 
 relogio = Relogio()
 thiago = Personagem()
@@ -10,7 +11,7 @@ if __name__ == '__main__':
 
         opcao = randint(1,4)
 
-        escolha = int(input(('''Escolha uma opção: 
+        escolha = int(input('''Escolha uma opção: 
         [ 1 ] Lazer
         [ 2 ] Trabalho
         [ 3 ] Estudo
@@ -18,8 +19,8 @@ if __name__ == '__main__':
         [ 5 ] Projetos pessoais\n
         O que você quer fazer? '''))
         
-        elif escolha == 4:
-            estudo == int(input('''\nEscolha  uma das opções de estudo:
+        if escolha == 3:
+            estudo = int(input('''\nEscolha  uma das opções de estudo:
         [1] HTML
         [2] PYTHON
         [3] JAVA SCRIPT
@@ -29,94 +30,94 @@ if __name__ == '__main__':
 
             if estudo == 1:
 
-                if thiago.estresse >= 50:
+                if thiago.xp >= 50:
                     
                     if opcao != 3:
                         relogio.avancaTempo(8)
-                        thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=20)
-                        print('Você fez uma boa escolha de horario, isso será favorável para seu aprendizado!')
+                        thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=10)
+                        print('Paranbens! Você conseguiu assimilar boa parte do que foi ensinado!')
 
                     else:
                         relogio.avancaTempo(4)
-                        thiago.atribuirValores(saude=-10, estresse=30, relacionamento=20, xp=-10)
-                        print('Seu esforço é notável, porém esse horário comprometerá seu desempenho.')
+                        thiago.atribuirValores(saude=-10, estresse=-30, relacionamento=-20, xp=-10)
+                        print('Puxa vida! Acabou a energia não vou conseguir assisitir a aula nem estudar!')
 
                 else:
-                    print('Você está estressado demais para assimilar o que irá estudar. Tente descansar para depois voltar aos estudos.')
+                    print('Continue estudando para somar cada vez mais conhecimento!')
                     continue
 
             elif estudo == 2:
 
-                if thiago.estresse >= 50:
+                if thiago.xp >= 20:
                     
                     if opcao != 3:
                         relogio.avancaTempo(8)
                         thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=20)
-                        print('Você fez uma boa escolha de horario, isso será favorável para seu aprendizado!')
+                        print('Você avançou em conhecimento, agora esta aprendendo uma nova liguagem!')
 
                     else:
                         relogio.avancaTempo(4)
-                        thiago.atribuirValores(saude=-10, estresse=30, relacionamento=20, xp=-10)
-                        print('Seu esforço é notável, porém esse horário comprometerá seu desempenho.')
+                        thiago.atribuirValores(saude=-10, estresse=+30, relacionamento=-20, xp=-10)
+                        print('Meu cachorro mastigou o cabo do meu monitor, não vou conseguir acompanhar a aula!')
 
                 else:
-                    print('Você está estressado demais para assimilar o que irá estudar. Tente descansar para depois voltar aos estudos.')
+                    print('Continue estudando para somar cada vez mais conhecimento!')
                     continue
 
             elif estudo == 3:
 
-                if thiago.estresse >= 50:
+                if thiago.xp >= 50:
 
                     if opcao != 3:
                         relogio.avancaTempo(8)
                         thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=20)
-                        print('Você fez uma boa escolha de horario, isso será favorável para seu aprendizado!')
+                        print('Olha só onde você conseguiu chegar! Continue com essa dedicação e terá muitas boas surpresas!')
 
                     else:
                         relogio.avancaTempo(4)
-                        thiago.atribuirValores(saude=-10, estresse=30, relacionamento=20, xp=-10)
-                        print('Seu esforço é notável, porém esse horário comprometerá seu desempenho.')
+                        thiago.atribuirValores(saude=-10, estresse=+30, relacionamento=-10, xp=-10)
+                        print('Puxa vida, derramei um copo de café no meu note e ele não que mais ligar!')
 
                 else:
-                    print('Você está estressado demais para assimilar o que irá estudar. Tente descansar para depois voltar aos estudos.')
+                    print('Continue estudando para somar cada vez mais conhecimento!')
                     continue
 
             elif estudo == 4:
 
-                if thiago.estresse >= 50:
+                if thiago.xp >= 50:
 
                     if opcao != 3:
                         relogio.avancaTempo(8)
                         thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=20)
-                        print('Você fez uma boa escolha de horario, isso será favorável para seu aprendizado!')
+                        print('Agora é hora de aplicar tudo que ja aprendeu, e iniciar uma nova joranda com mais uma linguagem!')
 
                     else:
                         relogio.avancaTempo(4)
-                        thiago.atribuirValores(saude=-10, estresse=30, relacionamento=20, xp=-10)
-                        print('Seu esforço é notável, porém esse horário comprometerá seu desempenho.')
+                        thiago.atribuirValores(saude=-30, estresse=+50, relacionamento=-20, xp=-10)
+                        print('Invadiram minha residencia e levaram todo meu equipamento de trabalho!')
 
                 else:
-                    print('Você está estressado demais para assimilar o que irá estudar. Tente descansar para depois voltar aos estudos.')
+                    print('Continue estudando para somar cada vez mais conhecimento!')
                     continue
 
             if estudo == 5:
 
-                if thiago.estresse >= 50:
+                if thiago.xp >= 50:
 
                     if opcao != 3:
                         relogio.avancaTempo(8)
                         thiago.atribuirValores(saude=20, estresse=-30, relacionamento=20, xp=20)
-                        print('Você fez uma boa escolha de horario, isso será favorável para seu aprendizado!')
+                        print('Olha só, somando cada vez mais conhecimento, agora chegou a hora de aumentar sua bagagem!')
 
                     else:
                         relogio.avancaTempo(4)
                         thiago.atribuirValores(saude=-10, estresse=30, relacionamento=20, xp=-10)
-                        print('Seu esforço é notável, porém esse horário comprometerá seu desempenho.')
+                        print('Precisei sair as pressas para levar minha esposa ao médico.')
 
                 else:
-                    print('Você está estressado demais para assimilar o que irá estudar. Tente descansar para depois voltar aos estudos.')
+                    print('Continue estudando para somar cada vez mais conhecimento!')
                     continue
                    
-    if thiago.saude == 0:
-        print('GAME OVER')
-        break
+        if thiago.saude == 0:
+            print('GAME OVER')
+            break
